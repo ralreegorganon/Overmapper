25b479f7936e6e2b4b38c1ea7ef44e230b50ebde
Jason Jones <jasonedwardjones@gmail.com>
2019-10-01 21:13:45 -0800

    empty

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Overmapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
